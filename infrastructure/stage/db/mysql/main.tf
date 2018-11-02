366b64216c64bf4ee00b64be2ac16ab508e18510
thenastypatty <44328267+thenastypatty@users.noreply.github.com>
2018-11-02 07:32:55 -0400

    Changing to use official RDS module (#3)
    * initial module commit
    
    * changed rds module to default terraform rds module
    
    * removed some extra parameters

--- infrastructure/stage/db/mysql/main.tf
+++ infrastructure/stage/db/mysql/main.tf
@@ -9,12 +9,45 @@ terraform {
   }
 }
 
-module "mysql_db" {
-  source = "../../../modules/mysql_db"
+#TODO: Change VPC
+data "aws_vpc" "default" {
+  default = true
+}
+#TODO: Change Subnet
+data "aws_subnet_ids" "all" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+#TODO: Change Security Group
+data "aws_security_group" "default" {
+  vpc_id = "${data.aws_vpc.default.id}"
+  name   = "default"
+}
+
+#TODO: Decide which engine version to use
+module "db" {
+  source = "terraform-aws-modules/rds/aws"
+  identifier = "${var.db_identifier}"
+  engine = "mysql"
+  engine_version = "5.7.19"
+  instance_class = "${var.aws_instance_class}"
   allocated_storage = "${var.allocated_storage}"
-  instance_class = "${var.instance_class}"
-  db_name = "${var.db_name}"
-  db_admin_username = "${var.db_admin_username}"
-  db_admin_password = "${var.db_admin_password}"
+  storage_encrypted = false
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  name = "${var.db_name}"
+  username = "${var.db_admin_username}"
+  password = "${var.db_admin_password}!"
+  port = "${var.db_connection_port}"
+
+  vpc_security_group_ids = ["${data.aws_security_group.default.id}"]
+  backup_retention_period = 1
+  tags = {
+    Environment = "staging"
+  }
+  enabled_cloudwatch_logs_exports = ["error", "general"]
+  subnet_ids = ["${data.aws_subnet_ids.all.ids}"]
   final_snapshot_identifier = "${var.final_snapshot_identifier}"
+  deletion_protection = true
+  options = []
 }
